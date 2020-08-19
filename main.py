e248d6af0867560021e4a151b4a723aa60100b52
Simon Duperray <simon.duperray@reseau.eseo.fr>
2020-08-19 17:16:55 +0200

    NotifySystem

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import psutil
 from win10toast import ToastNotifier
 
 bat = psutil.sensors_battery()
-toaster = ToastNotifier()
+notifier = ToastNotifier()
+appName = "KeepBatterySafe"
 
 def batteryLevel(bat):
     return bat.percent
@@ -11,12 +12,10 @@ def pluggedOrNot(bat):
     plugged = bat.power_plugged
     return True if plugged else False
 
-if(batteryLevel(bat)<23 and pluggedOrNot(bat)==False):
-    # notify
-    print("Plug your PC now !")
+if(batteryLevel(bat)<25 and pluggedOrNot(bat)==False):
+    message = "Plug your PC now ! " + str(batteryLevel(bat)) + "%"
+    notifier.show_toast(appName, message)
 
 if(batteryLevel(bat)==100 and pluggedOrNot(bat)==True):
-    # notify
-    print("Unplug your pc now !")
-
-toaster.show_toast("Hello world")
+    message = "Unplug your PC now ! " + str(batteryLevel(bat)) + "%"
+    notifier.show_toast(appName, message)
