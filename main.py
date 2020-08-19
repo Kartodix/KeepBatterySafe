6377b4476170b45c073b506e5818a28c0fe37f4c
Simon Duperray <simon.duperray@reseau.eseo.fr>
2020-08-19 19:51:53 +0200

    PopUp update

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import psutil
-from win10toast import ToastNotifier
+import ctypes  # An included library with Python install.   
 
 bat = psutil.sensors_battery()
-notifier = ToastNotifier()
 appName = "KeepBatterySafe"
 
 def batteryLevel(bat):
@@ -12,10 +11,17 @@ def pluggedOrNot(bat):
     plugged = bat.power_plugged
     return True if plugged else False
 
+# plug nom
 if(batteryLevel(bat)<25 and pluggedOrNot(bat)==False):
     message = "Plug your PC now ! " + str(batteryLevel(bat)) + "%"
-    notifier.show_toast(appName, message)
+    ctypes.windll.user32.MessageBoxW(0, message, appName, 1)
 
+# unplug now
 if(batteryLevel(bat)==100 and pluggedOrNot(bat)==True):
     message = "Unplug your PC now ! " + str(batteryLevel(bat)) + "%"
-    notifier.show_toast(appName, message)
+    ctypes.windll.user32.MessageBoxW(0, message, appName, 1)
+
+# prevent plug
+if(batteryLevel(bat)<=30 and pluggedOrNot(bat)==False):
+    message = str(batteryLevel(bat)) + "%! You soon should plug your PC!"
+    ctypes.windll.user32.MessageBoxW(0, message, appName, 1)
